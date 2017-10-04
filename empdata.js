3c92a372c547df30e219456ecddc779019439156
suijinyang <suij@seattleu.com>
2017-10-03 23:05:34 -0700

    hw1 commit

--- empdata.js
+++ empdata.js
@@ -1,12 +1,44 @@
 {
   "data": [
     {
-      "name": "Michael",
-      "designation": "Accountant",
-      "salary": "$170,850",
-      "joining_date": "2011/07/26",
-      "office": "Seoul",
-      "extension": "8432"
+      "team": "ManchesterCity",
+      "matchesplayed": "7",
+      "points": "19",
+      "losses": "0",
+      "wins": "6",
+      "draws": "1"
+    },
+    {
+      "team": "ManchesterUnited",
+      "matchesplayed": "7",
+      "points": "19",
+      "losses": "0",
+      "wins": "6",
+      "draws": "1"
+    },
+    {
+      "team": "TottenhamHotspur",
+      "matchesplayed": "7",
+      "points": "14",
+      "losses": "1",
+      "wins": "4",
+      "draws": "2"
+    },
+    {
+      "team": "Chelsea",
+      "matchesplayed": "7",
+      "points": "13",
+      "losses": "2",
+      "wins": "4",
+      "draws": "1"
+    },
+    {
+      "team": "Arsenal",
+      "matchesplayed": "7",
+      "points": "13",
+      "losses": "2",
+      "wins": "4",
+      "draws": "1"
     }
   ]
 }
